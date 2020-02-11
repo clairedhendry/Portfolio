7bbd8d7c5f736a94c3e0b23673b86357ceab0b2f
Claire Hendry <claire.d.hendry@gmail.com>
2020-02-11 10:55:22 -0500

    updated layout

---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict"
+
+{/* <section class="projects">
+<h2>Projects</h2>
+
+<div class="project first-project">
+<img/>
+<div id="icon1">img</div>
+<p>Project description and info</p>
+</div>
+
+<!--where new projects get added-->
+</section> */}
