5549df19cde7d4ecf5d4cfc52f8295d3a4a7d139
Claire Hendry <claire.d.hendry@gmail.com>
2020-08-06 10:36:08 -0400

    added to project descriptions

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const projectsArray = [
     {
         title: "Creative's Block",
         source: '<a href="https://creatives-block-client.vercel.app/" target=_"blank" rel="noopener"><img class="icon" src="images/creativesblock.png" alt="small icon of creatives block app" /></a>',
-        description: "A platform for artists and creatives. Post work in progress and get constructive feedback from a community of other creatives. Built with React, Node.js, Express, and PostgreSQL.",
+        description: "A platform for artists and creatives. Post work in progress and get constructive feedback from a community of other creatives. Inspired by my experience teaching college art classes and my own creative projects. Built with React, Node.js, Express, and PostgreSQL.",
         live: '<a href="https://creatives-block-client.vercel.app/" target=_"blank" rel="noopener">Live</a>',
         client_repo: '<a href="https://github.com/clairedhendrycreatives-block-client" target="_blank" rel="noopener">Client Repo</a>',
         api_repo: '<a href="https://github.com/clairedhendry/creatives-block-api" target="_blank" rel="noopener">API Repo</a>',
@@ -15,7 +15,7 @@ const projectsArray = [
     {
         title: "Escape to Nature",
         source: '<a href="https://nature-generator-client.now.sh/" target="_blank" rel="noopener"><img class="icon" src="images/escapetonature.png" alt="small icon of escape to nature app" /></a>',
-        description: "Choose a color to generate a slideshow of nature images with accompanying soundscape. Built with React, Node.js, Express, and PostgreSQL.",
+        description: "Choose a color to generate a slideshow of nature images with accompanying soundscape. Built for those in need of relaxing nature images and sounds. Built with React, Node.js, Express, and PostgreSQL.",
         live: '<a href="https://nature-generator-client.now.sh/" target="_blank" rel="noopener">Live</a>',
         client_repo: '<a href="https://github.com/clairedhendry/nature-generator-client" target="_blank" rel="noopener">Client Repo</a>',
         api_repo: '<a href="https://github.com/clairedhendry/nature-generator-api" target="_blank" rel="noopener">API Repo</a>',
@@ -24,7 +24,7 @@ const projectsArray = [
     {
         title: "Zodiac Quiz",
         source: '<a href="https://clairedhendry.github.io/zodiac-quiz/" target="_blank" rel="noopener"><img class="icon" src="images/zodiac-page.svg" alt="small icon of quiz app"/></a>',
-        description: "Test your knowledge of the Zodiac signs with this short, interactive quiz. Built with jQuery.",
+        description: "Test your knowledge of the Zodiac signs with this short, interactive quiz. For those with an interest in astrology. Built with jQuery.",
         live: '<a href="https://clairedhendry.github.io/zodiac-quiz/" target="_blank" rel="noopener">Live</a>',
         client_repo: '<a href="https://github.com/clairedhendry/zodiac-quiz" target="_blank" rel="noopener">Repo</a>',
         api_repo: '',
